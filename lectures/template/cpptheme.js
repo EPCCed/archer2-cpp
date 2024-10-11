468ff0247115fcebcd64cc007e95005cb6be236c
Nathan Mannall <n.mannall@epcc.ed.ac.uk>
2024-10-11 15:01:18 +0100

    Update intro chapter to use new module structure

--- lectures/template/cpptheme.js
+++ lectures/template/cpptheme.js
@@ -1,10 +1,16 @@
+import {epcc, Theme} from "https://EPCCed.github.io/remark_theme/latest.js";
 
 epcc.footer_text = "&copy; Rupert Nash, The University of Edinburgh, CC-BY";
-cpptheme = new Theme(
-    (str => str.substring(0, str.lastIndexOf("/")))(document.currentScript.src),
+var cpptheme = new Theme(
+    (str => str.substring(0, str.lastIndexOf("/")))(import.meta.url),
     '$BASEURL/style.css',
     {
-	thumb: function () {
-	    return '.thumb[\n.thumbtxt[\n' + this +'\n]\n]';
-	}
-    });
+        thumb: function () {
+            return '.thumb[\n.thumbtxt[\n' + this +'\n]\n]';
+        }
+    }
+);
+
+epcc.install();
+cpptheme.install();
+globalThis.slideshow = remark.create({sourceUrl: 'README.md'});
