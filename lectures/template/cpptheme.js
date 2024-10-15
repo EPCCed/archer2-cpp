285a27bb9cbd5164fb5effc18bd1b32eb1f16969
Nathan Mannall <n.mannall@epcc.ed.ac.uk>
2024-10-15 11:57:22 +0100

    Allow default footer text to be overridden

--- lectures/template/cpptheme.js
+++ lectures/template/cpptheme.js
@@ -1,6 +1,12 @@
 import {epcc, Theme} from "https://EPCCed.github.io/remark_theme/latest.js";
 
-epcc.footer_text = "&copy; Rupert Nash, The University of Edinburgh, CC-BY";
+var footer = new URL(import.meta.url).searchParams.get("footer");
+
+if (!footer) {
+    footer = "&copy; Rupert Nash, The University of Edinburgh, CC-BY";
+}
+epcc.footer_text = footer;
+
 var cpptheme = new Theme(
     (str => str.substring(0, str.lastIndexOf("/")))(import.meta.url),
     '$BASEURL/style.css',
