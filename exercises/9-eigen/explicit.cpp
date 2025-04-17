6c0d9dfbec370db60c3069298dc9e03cb31c56b8
nmannall <n.mannall@epcc.ed.ac.uk>
2025-04-17 12:10:57 +0100

    Update eigen exercise to match main branch

--- exercises/9-eigen/explicit.cpp
+++ exercises/9-eigen/explicit.cpp
@@ -6,40 +6,43 @@
 
 int main()
 {
-  int n = 12;
+    int n = 20;
+    int steps = 200;
+    std::vector<double> Avec(n * n);
 
-  std::vector<double> Avec(n * n);
 
-  Eigen::Map<Eigen::MatrixXd> A(Avec.data(), n, n);
-  A = Eigen::MatrixXd::Identity(n, n);
+    // Set up matrix A
+    Eigen::Map<Eigen::MatrixXd> A(Avec.data(), n, n);
+    A = Eigen::MatrixXd::Identity(n, n);
+    double delta = 0.4;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        A(i + 1, i) += delta;
+        A(i + 1, i + 1) += -delta;
 
-  double delta = 0.4;
+        A(i, i) += -delta;
+        A(i, i + 1) += +delta;
+    }
 
-  for (int i = 0; i < n - 1; ++i)
-  {
-    A(i + 1, i) += delta;
-    A(i + 1, i + 1) += -delta;
+    std::cout << "A = \n" << A << std::endl
+              << std::endl;
 
-    A(i, i) += -delta;
-    A(i, i + 1) += +delta;
-  }
 
-  std::cout << A << std::endl << std::endl;
+    // T_n
+    Eigen::VectorXd b(n);
+    b.setZero();
+    b.head(n / 2).array() = 1.0;
 
-  Eigen::VectorXd b(n);
-  b.setZero();
-  b.head(n / 2).array() = 1.0;
+    std::ofstream f;
+    f.open("explicit_sim.txt");
+    for (int i = 0; i < steps; ++i)
+    {
+        f << b.transpose() << std::endl;
+        // update time-step T_{n+1} = A.T_{n}
+        b = A * b;
+    }
 
-  std::ofstream f;
-  f.open("a.txt");
-  for (int i = 0; i < 20; ++i)
-  {
     std::cout << b.transpose() << std::endl;
-    f << b << std::endl << std::endl << std::endl;
-    b = A * b;
-  }
 
-  std::cout << b.transpose() << std::endl;
-
-  return 0;
+    return 0;
 }
--- exercises/9-eigen/implicit.cpp
+++ exercises/9-eigen/implicit.cpp
@@ -5,38 +5,42 @@
 
 int main()
 {
-  int n = 12;
+    int n = 20;
+    int steps = 100;
 
-  Eigen::MatrixXd A(n, n);
-  A = Eigen::MatrixXd::Identity(n, n);
+    // Set up matrix A
+    Eigen::MatrixXd A(n, n);
+    A = Eigen::MatrixXd::Identity(n, n);
 
-  double delta = -0.4;
+    double delta = -0.4;
 
-  for (int i = 0; i < n - 1; ++i)
-  {
-    A(i + 1, i) += delta;
-    A(i + 1, i + 1) += -delta;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        A(i + 1, i) += delta;
+        A(i + 1, i + 1) += -delta;
 
-    A(i, i) += -delta;
-    A(i, i + 1) += +delta;
-  }
+        A(i, i) += -delta;
+        A(i, i + 1) += +delta;
+    }
 
-  std::cout << A << std::endl << std::endl;
+    std::cout << "A = \n"
+              << A << std::endl
+              << std::endl;
 
-  Eigen::VectorXd b(n);
-  b.setZero();
+    Eigen::VectorXd b(n);
+    b.setZero();
 
-  b.head(n / 2).array() = 1.0;
+    b.head(n / 2).array() = 1.0;
 
-  std::ofstream f;
-  f.open("a.txt");
-  for (int i = 0; i < 20; ++i)
-  {
+    std::ofstream f;
+    f.open("implicit_sim.txt");
+    for (int i = 0; i < 200; ++i)
+    {
+        // Solve A.T_{n+1} = T_{n} for T_{n+1}
+        f << b.transpose() << std::endl;
+        b = A.colPivHouseholderQr().solve(b);
+    }
+
     std::cout << b.transpose() << std::endl;
-    f << b << std::endl << std::endl << std::endl;
-    b = A.colPivHouseholderQr().solve(b);
-  }
-
-  std::cout << b.transpose() << std::endl;
-  return 0;
+    return 0;
 }
